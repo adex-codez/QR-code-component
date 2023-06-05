d420f31ded57969214741f57da6a86a41133488e
Adegbite Adeadura <adetimilehin@gmail.com>
2023-06-05 14:05:47 +0100

    added readme

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
         primary: 'hsl(212, 45%, 89%)',
         secondary: 'hsl(220, 15%, 55%)',
         tertiary: 'hsl(218, 44%, 22%)'
+      },
+      fontFamily: {
+        body: 'Outfit, sans-serif;'
       }
     },
   },
